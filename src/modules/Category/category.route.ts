db8f1312723f11149fc9c3d98b0b542c82442bc7
iamsondev <sondipkumarsk@gmail.com>
2026-03-27 13:27:26 +0600

    modifie category validation and  modifie the category route

--- src/modules/Category/category.route.ts
+++ src/modules/Category/category.route.ts
@@ -2,12 +2,15 @@ import { Router } from 'express';
 import auth from '../../middlewares/auth';
 import { Role } from '../../generated/prisma/enums';
 import { CategoryControllers } from './category.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { CategoryValidation } from './category.validation';
 
 const router = Router();
 
 router.post(
   '/',
   auth(Role.ADMIN),
+  validateRequest(CategoryValidation.createCategoryValidationSchema),
   CategoryControllers.createCategory,
 );
 
