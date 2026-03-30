cd48fb12d1dccd39e04a8580dd088deb786b33b1
iamsondev <sondipkumarsk@gmail.com>
2026-03-30 23:30:05 +0600

    feat: set accessToken and refreshToken as HttpOnly cookies on login

--- src/modules/Auth/auth.controller.ts
+++ src/modules/Auth/auth.controller.ts
@@ -20,8 +20,17 @@ const loginUser = catchAsync(async (req: Request, res: Response) => {
   const { refreshToken, accessToken } = result;
 
   res.cookie('refreshToken', refreshToken, {
-    secure: process.env.NODE_ENV === 'production',
     httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    maxAge: 1000 * 60 * 60 * 24 * 30, // 30 days
+  });
+
+  res.cookie('accessToken', accessToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    maxAge: 1000 * 60 * 60 * 24, // 1 day
   });
 
   sendResponse(res, {
